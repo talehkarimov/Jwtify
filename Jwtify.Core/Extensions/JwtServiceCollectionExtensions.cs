64b49ff8b40aef68f3e31b351c535669caa5008d
taleh.karimov <taleh.karimov@smartsolutions.az>
2025-02-23 21:08:50 +0400

    update dependency injections

--- Jwtify.Core/Extensions/JwtServiceCollectionExtensions.cs
+++ Jwtify.Core/Extensions/JwtServiceCollectionExtensions.cs
@@ -1,6 +1,8 @@
 ﻿using Jwtify.Core.Abstractions;
 using Jwtify.Core.Builders;
+using Jwtify.Core.Enums;
 using Jwtify.Core.Services;
+using Jwtify.Core.Strategies;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Jwtify.Core.Extensions
@@ -14,6 +16,18 @@ namespace Jwtify.Core.Extensions
             var options = builder.Build();
 
             services.AddSingleton(options);
+
+            if (options.Algorithm == SigningAlgorithm.HS256)
+            {
+                services.AddSingleton<ISigningStrategy>(provider =>
+                    new HmacSha256Strategy());
+            }
+            else if (options.Algorithm == SigningAlgorithm.RS256)
+            {
+                services.AddSingleton<ISigningStrategy>(provider =>
+                    new RsaStrategy()); 
+            }
+
             services.AddScoped<IJwtHelper, JwtHelper>();
             services.AddScoped<ITokenGenerator, TokenGenerator>();
             services.AddScoped<ITokenValidator, TokenValidator>();
