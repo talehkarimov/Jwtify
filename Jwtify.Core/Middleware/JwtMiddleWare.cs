ccf59859b61424f025c0e1092c2b6004f8fe62b7
taleh.karimov <taleh.karimov@smartsolutions.az>
2025-02-23 22:41:48 +0400

    update middleware

--- Jwtify.Core/Middleware/JwtMiddleWare.cs
+++ Jwtify.Core/Middleware/JwtMiddleWare.cs
@@ -17,32 +17,45 @@ public class JwtMiddleware
     public async Task Invoke(HttpContext context, IJwtHelper jwtHelper, IRefreshTokenService refreshTokenService)
     {
         var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
-        if (token != null && jwtHelper.ValidateToken(token, out var claims))
+
+        if (string.IsNullOrEmpty(token) || !jwtHelper.ValidateToken(token, out var claims))
         {
-            context.Items["User"] = claims;
-
-            if (_jwtOptions.EnableRefreshToken)
-            {
-                var refreshToken = context.Request.Headers["Refresh-Token"].FirstOrDefault();
-                if (refreshToken != null && refreshTokenService.ValidateRefreshToken(refreshToken))
-                {
-                    var tokenResult = jwtHelper.GenerateToken(claims);
-                    context.Response.Headers["New-Access-Token"] = tokenResult.AccessToken;
-
-                    if (!string.IsNullOrEmpty(tokenResult.RefreshToken))
-                    {
-                        context.Response.Headers["New-Refresh-Token"] = tokenResult.RefreshToken;
-                    }
-                }
-                else
-                {
-                    context.Response.StatusCode = 400;
-                    await context.Response.WriteAsync("Invalid refresh token");
-                    return;
-                }
-            }
+            context.Response.StatusCode = 400;
+            await context.Response.WriteAsync("Invalid authorization token");
+            return;
         }
 
+        context.Items["User"] = claims;
+
+        if (_jwtOptions.EnableRefreshToken)
+        {
+            var refreshToken = context.Request.Headers["Refresh-Token"].FirstOrDefault();
+
+            if (string.IsNullOrEmpty(refreshToken))
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Refresh token is missing.");
+                return;
+            }
+
+            if (refreshTokenService.ValidateRefreshToken(refreshToken))
+            {
+                var tokenResult = jwtHelper.GenerateToken(claims);
+                context.Response.Headers["New-Access-Token"] = tokenResult.AccessToken;
+
+                if (!string.IsNullOrEmpty(tokenResult.RefreshToken))
+                {
+                    context.Response.Headers["New-Refresh-Token"] = tokenResult.RefreshToken;
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync("Invalid refresh token");
+                return;
+            }
+        }
         await _next(context);
     }
+
 }
